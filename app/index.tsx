02d38780ee2b495c3080d02d02b0ac06d6db0aa7
unknown <lab3124@etecjk.local>
2025-09-25 20:36:58 -0300

    Hello World !

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Home() {
+    // bloco de codigo main
+    return (
+        <View>
+            <Text> Meu Primeiro App</Text>
+        </View> 
+    );
+}
