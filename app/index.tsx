0fb90ca88be2fcecdc26d20538fc7f9dc19c4d77
unknown <lab3124@etecjk.local>
2025-09-25 22:13:49 -0300

    Hello World !

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,48 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Home() {
     // bloco de codigo main
-    return (
-        <View>
-            <Text> Meu Primeiro App</Text>
+    const [name,setName] = useState('')// atribuição por descontruição
+    const [idade,setIdade] = useState(parseInt(''))
+    const [peso,setPeso] = useState(parseFloat(''))
+    const [altura,setAltura] =useState(parseFloat(''))
+    const IMC = peso/(altura*altura)
+    
+    return ( //interface grafica que mostra a tela pro usuario
+        <View style={styles.body} >
+
+            <Text style={styles.title} >
+                 Hello World !!
+            </Text>
+
+            <TextInput 
+                placeholder=" Digite seu Nome: "
+                onChangeText ={setName}/>
+            <TextInput 
+                placeholder=" Digite sua Idade: "
+                onChangeText ={setIdade}/>
+            <TextInput 
+                placeholder=" Digite sua Peso: "
+                onChangeText ={setPeso}/>
+            <TextInput 
+                placeholder=" Digite sua Altura: "
+                onChangeText = {setAltura}/>
+
+            <Text style={styles.title} >
+                 Olá, {name} boa noite,seus dados são Idade: {idade} Altura: {altura} Peso: {peso} IMC: {IMC} 
+            </Text>
+
         </View> 
     );
 }
+
+const styles = StyleSheet.create({
+    body: {
+        backgroundColor: "red", 
+        padding: "auto" 
+    }, 
+    title: {
+        fontSize: 19 
+    }
+})
